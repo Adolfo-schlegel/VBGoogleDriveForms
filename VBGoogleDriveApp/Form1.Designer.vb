309fec1422369abeee2784f3bad9ac2f4b86bd9b
Adolfo-schlegel <adolfo.77@outlook.es>
2022-01-06 17:22:38 -0300

    Inicio

---
+++ VBGoogleDriveApp/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSubir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FilePath = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(12, 12)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(108, 23)
+        Me.btnBuscar.TabIndex = 0
+        Me.btnBuscar.Text = "Buscar Archivo"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnSubir
+        '
+        Me.btnSubir.Location = New System.Drawing.Point(316, 196)
+        Me.btnSubir.Name = "btnSubir"
+        Me.btnSubir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubir.TabIndex = 1
+        Me.btnSubir.Text = "Subir"
+        Me.btnSubir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ruta de tu archivo ->"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(136, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 15)
+        Me.Label2.TabIndex = 3
+        '
+        'FilePath
+        '
+        Me.FilePath.AutoSize = True
+        Me.FilePath.Location = New System.Drawing.Point(136, 61)
+        Me.FilePath.Name = "FilePath"
+        Me.FilePath.Size = New System.Drawing.Size(0, 15)
+        Me.FilePath.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(403, 231)
+        Me.Controls.Add(Me.FilePath)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSubir)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnSubir As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FilePath As Label
+End Class
